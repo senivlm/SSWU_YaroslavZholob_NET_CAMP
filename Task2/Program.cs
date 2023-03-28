55fc51137714583dcbab4f05ae416156d60515e2
Yaroslav Zholob <73608684+ZholobYaroslav@users.noreply.github.com>
2023-03-28 15:11:58 +0200

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using SigmaHomeTask1.Task1;
+using SigmaHomeTask1.Task2;
+
+namespace SigmaHomeTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task Longest Line");
+            MatrixLongestLine matrixLongestLine = new MatrixLongestLine(5,10);
+            matrixLongestLine.Initialize();
+            Console.WriteLine(matrixLongestLine.ToString());
+            matrixLongestLine.FindTheLongestString();
+        }
+    }
+}
