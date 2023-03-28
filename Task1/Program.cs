c024df425e51eeb54a14f9dc9262b45eb2c23474
Yaroslav Zholob <73608684+ZholobYaroslav@users.noreply.github.com>
2023-03-28 15:11:01 +0200

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using SigmaHomeTask1.Task1;
+using SigmaHomeTask1.Task2;
+
+namespace SigmaHomeTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task Spiral Snake\nEnter rows number:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter columns number:");
+            int m = int.Parse(Console.ReadLine());
+            MatrixSpiralSnake matrixSpiralSnake = new MatrixSpiralSnake(n,m);
+            matrixSpiralSnake.SpiralSnake(1);
+            Console.WriteLine(matrixSpiralSnake.ToString());
+            matrixSpiralSnake.SpiralSnakeReverse(1);
+            Console.WriteLine(matrixSpiralSnake.ToString());
+        }
+    }
+}
